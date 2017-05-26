e99fd943c511ed6e61407361d6a3f246e0eedde1
PRR78 <pooryarajaee78@gmail.com>
2017-05-26 12:13:25 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -90,28 +90,27 @@ function create_config( )
 		"fun",
 
 	},
-    sudo_users = {111334847, 157059515, sudo_id},
+    sudo_users = {270541145, 226123856, sudo_id},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn v6.0
-An advanced administration bot based on https://valtman.name/telegram-cli
-
-》https://github.com/BeyondTeam/BDReborn 
-
-》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer《
-》@ToOfan ➣ Developer《
-
+    info_text = [[》TelePlus V5.5
+An advanced administration bot based on Beyond
+		
+》Admin :
+》@PHAE2099 ➣ Sudo《
+		
+》Messenger Bots :
+》@PtelePlus_bot
+》@PTelePlus2_bot
+		
 》Special thanks to :
-》Beyond Team Members
-
+》#Beyond_Source
+》#Mr_Farzad
+》#Prince
+		
 》Our channel :
-》@BeyondTeam《
-
-》Our website :
-》http://BeyondTeam.ir
+》@TelePlus《
 ]],
   }
 	serialize_to_file(config, './data/config.lua')
